36f0c2fcf60bb16924999bde02a59a4059986f2d
jjj <john.juul.jensen@hotmail.com>
2024-03-04 14:46:45 +0100

    somewhat working shear rotate, except it's off-center at 90 degrees. Naively correcting by 0.5 does not work.

--- main.ts
+++ main.ts
@@ -1,14 +1,62 @@
 let rot = 0
+let rotDir = 1
 tiles.setCurrentTilemap(tilemap`level1`)
 let myImage = assets.image`box`
+let rotImage = myImage.clone();
+rotImage.flipX();
+rotImage.flipY();
+
+tiles.placeOnTile(sprites.create(myImage, SpriteKind.Food), tiles.getTileLocation(1,1));
+tiles.placeOnTile(sprites.create(myImage.transposed(), SpriteKind.Food), tiles.getTileLocation(2,1));
+tiles.placeOnTile(sprites.create(rotImage, SpriteKind.Food), tiles.getTileLocation(3,1));
+
+
 let mySprite = sprites.create(myImage, SpriteKind.Player)
-controller.moveSprite(mySprite)
+tiles.placeOnTile(mySprite, tiles.getTileLocation(2,4));
+
+
+//controller.moveSprite(mySprite)
 scene.cameraFollowSprite(mySprite)
-tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundSouthEast1)
-game.onUpdateInterval(10, function () {
-    rot += 2
-    if (rot >= 90) {
-        rot = 0
-    }
-    mySprite.setImage(pathing.shearX(Math.tan(Math.PI * (rot / 180)), myImage))
+//tiles.placeOnRandomTile(mySprite, sprites.dungeon.darkGroundSouthEast1)
+
+function rotate(amount: number) {
+    rot += amount
+    // if (rot > 180) {
+    //     rot = 180;
+    // }
+    // if (rot < -90) {
+    //     rot = -90;
+    // }
+    mySprite.setImage(imagesExt.rotate(imagesExt.RotationType.ShearRotate, rot, myImage))
+    mySprite.sayText(`${rot}`)
+}
+
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotate(-1);
 })
+controller.left.onEvent(ControllerButtonEvent.Repeated, function () {
+    rotate(-1);
+})
+
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    rotate(1);
+})
+controller.right.onEvent(ControllerButtonEvent.Repeated, function () {
+    rotate(1);
+})
+
+
+// game.onUpdateInterval(10, function () {
+//     rot += rotDir
+//     if (rot > 180) {
+//         rot = 180;
+//         rotDir = -1 * rotDir;
+//     }
+//     if (rot < -90) {
+//         rot = -90;
+//         rotDir = -1 * rotDir;
+//     }
+//     mySprite.setImage(imagesExt.rotate(imagesExt.RotationType.ShearRotate, rot, myImage))
+//     //mySprite.setImage(myImage.rotated(90));
+//     mySprite.sayText(`${rot}`)
+// })
--- pathing.ts
+++ pathing.ts
@@ -1,40 +1,4 @@
-
 namespace pathing {
-
-    //% block="shear $img by $amount"
-    //% img.shadow=screen_image_picker
-    export function shearX(amount: number, img: Image): Image {
-        const gradient = amount / img.height;
-        let res = image.create(img.width, img.height);
-        for (let y = 0, j = - img.height / 2; y < img.height; ++y, ++j) { 
-            for (let x = 0; x < img.width; ++x) {
-                const offset = gradient * j;
-                res.setPixel(x + offset,y, img.getPixel(x,y));
-            }
-        }
-        return res;
-    }
-
-    export function shearY(amount: number, img: Image): Image {
-        const gradient = amount / img.width;
-        let res = image.create(img.width, img.height);
-        for (let x = 0, j = - img.width / 2; x < img.width; ++x, ++j) {
-            for (let y = 0; y < img.height; ++y) { 
-                const offset = gradient * j;
-                res.setPixel(x ,y + offset, img.getPixel(x,y));
-            }
-        }
-        return res;
-    }
-
-    /* Forward rotation by shearing
-    shear in X by -tan(angle/2)
-    shear in Y by sin(angle)
-    shear in X by -tan(angle/2)
-
-
-
-    */
 
     
 }
